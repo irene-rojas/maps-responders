a9d289633287c9629aee957609d427556f33b501
Irene R <irene@smallwondermedia.com>
2019-04-30 01:01:25 -0400

    backup

--- src/App.js
+++ src/App.js
@@ -19,21 +19,23 @@ class App extends Component {
         navigator.geolocation.getCurrentPosition((position) => { 
             this.setState({
                 lat: position.coords.latitude.toFixed(4),
-                lng: position.coords.longitude.toFixed(4),
+                lng: position.coords.longitude,
             }, () => {
                 console.log(this.state.lat, this.state.lng);
+                this.getMap();
             }); 
         });
     }
     
     getMap = () => {
-        mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaXJlbmVyb2phcyIsImEiOiJjanYzNmk3MXkwNGZxM3ludzdqcjRnNWVyIn0.K6kZ5Mxbwn7TZbocBF4F0A';
         let map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [this.state.lat, this.state.lng],
             zoom: 13
             });
+            return map;
     }
 
 
